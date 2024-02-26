d1044d5ff7cf0fac6065b39d9c8981d560d2ceb0
jcarpio <jose.carpio@gmail.com>
2024-02-26 13:00:56 +0100

    Create rain_first_order.pl

---
+++ rain_first_order.pl
@@ -0,0 +1,11 @@
+% if is_raining -> tomatoes_grow.
+% if is_raining -> potatoes_grow.
+% if is_raining -> 
+
+% grow( <whatever> ).
+
+% grow(Whatwever) 
+% it is true when Whatwever grows.
+
+% if is_raning -> grow(tomatoes).
+% if is_raning -> grow(potatoes).
