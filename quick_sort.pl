b4956a54899c93380e2c5687215bdaac1380c196
jcarpio <jose.carpio@gmail.com>
2024-03-18 14:25:54 +0100

    Update quick_sort.pl

--- quick_sort.pl
+++ quick_sort.pl
@@ -1,12 +1,24 @@
+:- set_prolog_stack(global, limit(16 000 000 000)).
+:- set_prolog_stack(trail,  limit(16 000 000 000)).
+:- set_prolog_stack(local,  limit(16 000 000 000)).
+
 /*
 
 quick_sort(+List, -Result)
   is true if Result unify with a list with same elements
-  that contai List in ascendent order
+  that contains List in ascendent order
   
-   insert_sort([1,3,2,5,4,6,7,8],R).
+   quick_sort([1,3,2,5,4,6,7,8],R).
    R = [1, 2, 3, 4, 5, 6, 7, 8] .
 */
+
+quick_sort([], []).
+
+quick_sort([Head|Tail], R):-
+   division(Head, Tail, Less, High),
+   quick_sort(Less, RL),
+   quick_sort(High, RH),
+   append(RL, [Head|RH], R).
 
 /*
 division(+Pivot, +List, -Less, -High).
@@ -15,7 +27,10 @@ division(+Pivot, +List, -Less, -High).
   the elements in List that are great than Pivot.
 */
 
-division(_, [], [], []).
+division( _, [], [], []).
 
-division(Pivot, [Head|Tail],  ,  ):-
-  division(Pivot, Tail, Less, High),
+division(Pivot, [Head|Tail], [Head|Less], High):- Head =< Pivot,
+   division(Pivot, Tail, Less, High).
+  
+division(Pivot, [Head|Tail], Less, [Head|High]):- Head > Pivot,
+   division(Pivot, Tail, Less, High).
