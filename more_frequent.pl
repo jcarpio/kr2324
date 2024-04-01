795483ad37cd5ab3f52044bb65ff7b4309eafc74
jcarpio <jose.carpio@gmail.com>
2024-04-01 14:26:50 +0200

    Create more_frequent.pl

---
+++ more_frequent.pl
@@ -0,0 +1,11 @@
+/*
+
+more_frequent(+List, -Elem, -N)
+  is true if Elem unify with the element is List that are more frequent
+  and N is the number of times Eleme appear in List.
+  
+  more_frequent([1,1,2,2,3,1,3,4,5], E, N).
+  E = 1
+  N = 3
+
+*/
