fd5e2fed7674069aa2489bf0c07dd36922017336
jcarpio <jose.carpio@gmail.com>
2024-05-03 12:27:10 +0200

    Update cannibals.pl

--- cannibals.pl
+++ cannibals.pl
@@ -1,65 +1,123 @@
-
 /*
-
-1. Describe State
- 
-   state(MR, CR, BoatPlace)
-   
-   Put inside state only variable information. For example, we don't write in state
-   that the maximum number of people in boat are two, because that is commun to all
-   states. At the same time, boat can't travel alone.
-   
-2. Describe Initial and Final states
-
-   Initial state = state(3, 3, right)
-   
-   Final state   = state(0, 0, left)
-   
-3. Describe Movements
-
-*/
-
-   mov(move(M, 0, left), state(MR, CR, right), state(NewMR, CR, left) ):- MR > 0,
-      MR  >= M, NewMR is MR - M.
+  State Problem
+  
+  Missionaries and Cannibals
+  
+  1. Tree Missionaires and 3 cannibales in right side
+     of the river
+  2. Only one boat with capacity for two people
+  3. At any side of the river, the number of Cannibals
+     can not be higher than the number of Missionaires
 	  
-   mov(move(0, C, left), state(MR, CR, right), state(MR, NewCR, left) ):- CR > 0,
-      CR >= C, NewCR is CR - C.
-	  
-   mov(move(M, 0, right), state(MR, CR, left), state(NewMR, CR, right) ):- ML is 3 - MR,
-      ML > 0,
-      ML  >= M, NewMR is MR + M.
-	  
-   mov(move(0, C, right), state(MR, CR, left), state(MR, NewCR, right) ):- CL is 3 - CR,
-      CL  > 0,
-      CL >= C, NewCR is CR + C.	  
-
-
-/*
-	  
-4. Define a valid state 	  
-
-*/
-    valid(state(0, CR, _)):- CR =< 3.
-	valid(state(MR, 0, _)):- MR = 0; MR = 3.
-	valid(state(MR, CR, _)):- ML is 3- MR, CL is 3 - CR, MR > 0, MR >= CR, ML >= CL.
-	
-/*
-
- 5. path(+Ini, +Fin, +Visited, -Path)
- 
-*/
-	
-	
-   path(Ini, Ini, _, []).	
+	Solution: 	
+  [move(1,1,left),
+   move(1,0,right),
+   move(0,2,left),
+   move(0,1,right),
+   move(2,0,left),
+   move(1,1,right),
+   move(2,0,left),
+   move(0,1,right),
+   move(0,2,left),
+   move(1,0,right),
+   move(1,1,left)]
    
-   path(Ini, Fin, Visited, [Mov|Path]):-
-     mov(Mov, Ini, TMP),
-	 valid(TMP),
-	 \+ member(TMP, Visited),
-	 path(TMP, Fin, [TMP|Visited], Path).
+   
+	 #Cannibals =< #Missionaries 
 	 
+	 Otherwise Cannibals can eat the Missionaries
+	
+    State Problem
+	
+	1. State
+	
+	First approach
+	state(ML, CL, MR, CR, Boat).
+	
+	Second approach
+	state(ML, CL, Boat).
+	
+	MR = 3 - ML
+    CR = 3 - CL	
+	
+	2. Movements
+	
+	mov(MovName, StateBefore, StateAfter)
+	
+	
+	  2.1. Move one missionaire to the left
+	  2.2. Move one cannibal to the left
+	  2.3. Move one missionaire to the right
+	  2.4. Move one cannibal to the right
+	  2.5. Move two missionaire to the left
+	  2.6. Move two cannibals to the left
+      2.7. Move two missionaire to the right
+	  2.8. Move two cannibals to the right
+      2.9. Move one mis and one cannibals to the right
+      2.10. Move one mis and one cannibals to the left
+	  
+	  state(ML, CL, Boat).
+	  
+	First approach  
+mov(oneMisLeft, state(ML, CL, right), state(ML2, CL)):-
+   MR is 3 - ML,
+   MR > 0,
+   ML2 is ML + 1. 
+   Second approach
+*/
 
+   mov(move(M, C, left), state(ML, CL, right), 
+     state(ML2, CL2, left)):-
+     ML2 is ML + M, ML2 >= 0, ML2 =< 3,
+     CL2 is CL + C, CL2 >= 0, CL2 =< 3.
 
-move(M, C, _):- member(M, [0,1,2]), C is 2 - M.
+   mov(move(M, C, right), state(ML, CL, left), 
+     state(ML2, CL2, right)):-
+     ML2 is ML - M, ML2 >= 0, ML2 =< 3, 
+     CL2 is CL - C, CL2 >= 0, CL2 =< 3.	 
+	 
+   move(1,1,_).
+   move(2,0,_).
+   move(0,2,_).
+   move(1,0,_).
+   move(0,1,_).
+
+   not_valid(state(2,3, _)).
+   not_valid(state(1,3, _)).
+   not_valid(state(1,2, _)).
+   not_valid(state(2,1, _)). 
+   not_valid(state(1,0, _)).
+   not_valid(state(2,0, _)).
+  
+	
+/*	
+   3. Path to the solution
+   path(+InitialState, +FinalState, +Visited, -Path)
+   this is true if Path unify with the list of
+   movements to go from InitialState to FinalState
+   witout repeating visited states.
+   
+   Path = [move(1,0,left), move(1,0, right), ...]
+   
+   
+   Ini  -----------------------> Final
+              Path
+			  
+   Ini -> mov -> Aux
+                   ------------> Final
+	                    Path
+                        N-1
+   */
+   
+   path(Ini, Ini, _, []).
+   
+   path(Ini, Fin, Visited, [move(M, C, Dir)|Path]):-
+     move(M, C, Dir),
+     mov(move(M, C, Dir), Ini, Aux),
+     \+ not_valid(Aux),
+	 \+ member(Aux, Visited),
+     path(Aux, Fin, [Aux|Visited], Path).
+
+solution(Path):- path(state(0,0,right), state(3,3,_), [] ,Path).
 	 
 	
