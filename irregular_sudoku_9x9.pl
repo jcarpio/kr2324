7a758cbfadad242371c9568045e0796254f7713a
jcarpio <jose.carpio@gmail.com>
2024-05-06 14:13:07 +0200

    Create irregular_sudoku_9x9.pl

---
+++ irregular_sudoku_9x9.pl
@@ -0,0 +1,22 @@
+:- use_module(library(clpfd)).
+
+sudoku(Rows) :-
+        length(Rows, 9),
+        maplist(same_length(Rows), Rows),
+        append(Rows, Vs), Vs ins 1..9,
+        maplist(all_distinct, Rows),
+        transpose(Rows, Columns),
+        maplist(all_distinct, Columns),
+        Rows = [R1,R2,R3,R4,R5,R6,R7,R8,R9],
+		R1 = [E11, E12, E13, E14 ,E15, E16, E17, E18, E19],
+ 		R2 = [E21, E22, E23, E24 ,E25, E26, E27, E28, E29],
+ 		R3 = [E31, E32, E33, E34 ,E35, E36, E37, E38, E39],		
+ 		R4 = [E41, E42, E43, E44 ,E45, E46, E47, E48, E49],		
+ 		R5 = [E51, E52, E53, E54 ,E55, E56, E57, E58, E59],		
+ 		R6 = [E61, E62, E63, E64 ,E65, E66, E67, E68, E69],		
+ 		R7 = [E71, E72, E73, E74 ,E75, E76, E77, E78, E79],		
+ 		R8 = [E81, E82, E83, E84 ,E85, E86, E87, E88, E89],
+ 		R9 = [E91, E92, E93, E94 ,E95, E96, E97, E98, E99],		
+		all_distinct([E11,E12,E13,E21,E22,E31,E32,E33,E42]),
+		all_distinct([E14,E15,E16,E23,E24,E25,E35,E36,E45]),
+		all_distinct([E17,E18,E19,E26,E27,E28,E29,E37,E39]),
