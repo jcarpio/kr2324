bee32efe2b66a5275ff5da4f29268a93013c9f0a
jcarpio <jose.carpio@gmail.com>
2024-05-13 14:31:11 +0200

    Create example1.pl

---
+++ example1.pl
@@ -0,0 +1,16 @@
+slots_per_week(35).
+
+slots_per_day(7).
+
+req_with_slots(R, R-Slots) :- R = req(_,_,_,N), length(Slots, N).
+
+class_subject_teacher_times('1a', deu, sjk1, 4).
+class_subject_teacher_times('1a', mat, mat1, 5).
+class_subject_teacher_times('1a', eng, anj1, 3).
+class_subject_teacher_times('1a', h, zgo1, 2).
+class_subject_teacher_times('1a', geo, geo1, 2).
+class_subject_teacher_times('1a', ch, kem1, 2).
+class_subject_teacher_times('1a', bio, bio1, 2).
+class_subject_teacher_times('1a', ph, fiz1, 2).
+class_subject_teacher_times('1a', f, rai1, 2).
+class_subject_teacher_times('1a', lat, atvz1, 3).
