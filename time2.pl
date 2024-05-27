806fdce35624b4b5423c73560b996b630e114467
jcarpio <jose.carpio@gmail.com>
2024-05-27 14:24:20 +0200

    Update time2.pl

--- time2.pl
+++ time2.pl
@@ -34,7 +34,13 @@ requirements_variables(Rs, Vars) :-
         maplist(constrain_teacher(Rs), Teachers),
         maplist(constrain_class(Rs), Classes),
         maplist(constrain_room(Rs), Rooms).		
+/*
+Constrain Subject
+  - By default, lessons have diffent days of the week,
+    with the exception of coupling lessons.
+  
 
+*/
 constrain_subject(req(Class,Subj,_Teacher,_Num)-Slots) :-
         strictly_ascending(Slots), % break symmetry
         maplist(slot_quotient, Slots, Qs0),
@@ -45,12 +51,94 @@ constrain_subject(req(Class,Subj,_Teacher,_Num)-Slots) :-
         list_without_nths(Qs0, Seconds, Qs),
         strictly_ascending(Qs).
 		
+/* list_without_nths(+List, +Remove, -R)
+      it is true if R unify with a list that contains
+	  elements of Lists without elements that ocupied
+	  positions in Remove starting with 0.	
+*/	
+
+list_without_nths(List, [], List).
+list_without_nths(List, [Head|Tail], R):-
+  list_without_nths(List, Tail, RTail),
+  remove_pos(Head, RTail, R).
+  
+/* remove_pos(+Pos, +List,  -R).
+      it is true if R unify with a list with
+      the elements of List without elem at 
+      position Pos.	 
+*/
+  
+remove_pos(0, [_|Tail], Tail).  
+  
+remove_pos(Pos, [Head|Tail], [Head|R]):- Pos > 0,
+   Pos2 is Pos - 1,
+   remove_pos(Pos2, Tail, R).  
+  
+  
+%:- list_without_nths("abcd", [3], "abc").
+%:- list_without_nths("abcd", [1,2], "ad").
+
+slots_couplings(Slots, F-S) :-
+        nth0(F, Slots, S1),
+        nth0(S, Slots, S2),
+        S2 #= S1 + 1.		
+		
 strictly_ascending(Ls) :- chain(Ls, #<).
 
+
+constrain_teacher(Rs, Teacher) :-
+        % tfilter(teacher_req(Teacher), Rs, Sub),
+		filter_req_teacher(Rs, Teacher, Sub),
+        pairs_slots(Sub, Vs),
+        all_different(Vs),
+        findall(F, teacher_freeday(Teacher, F), Fs),
+        maplist(slot_quotient, Vs, Qs),
+        maplist(all_diff_from(Qs), Fs).
+
+teacher_req(T0, req(_C,_S,T1,_N)-_, T) :- =(T0,T1,T).
+
+all_diff_from(Vs, F) :- maplist(#\=(F), Vs).
+
+/*
+filter_req_teacher(+Req, +Teacher, -R)
+
+
+*/
+
+filter_req_teacher([], _, []).
+
+filter_req_teacher([req(C,S,Teacher,N)-Slots|Tail], Teacher, [req(C,S,Teacher,N)-Slots|R]):-   
+   filter_req_teacher(Tail, Teacher, R).
+   
+filter_req_teacher([req(_C,_S,TeacherList,_N)-_Slots|Tail], Teacher, R):-  
+   TeacherList \= Teacher, 
+   filter_req_teacher(Tail, Teacher, R).   
+
+
+constrain_class(Rs, Class) :-
+        tfilter(class_req(Class), Rs, Sub),
+        pairs_slots(Sub, Vs),
+        all_different(Vs),
+        findall(S, class_freeslot(Class,S), Frees),
+        maplist(all_diff_from(Vs), Frees).   
+   
+   
+constrain_room(Reqs, Room) :-
+        findall(r(Class,Subj,Less), room_alloc(Room,Class,Subj,Less), RReqs),
+        maplist(sameroom_var(Reqs), RReqs, Roomvars),
+        all_different(Roomvars).
+		
+sameroom_var(Reqs, r(Class,Subject,Lesson), Var) :-
+        memberchk(req(Class,Subject,_Teacher,_Num)-Slots, Reqs),
+        nth0(Lesson, Slots, Var).		
+   
 % 
 slot_quotient(S, Q) :-
         slots_per_day(SPD),
         Q #= S // SPD.		
+
+
+%----------------------------------------
 
 slots_per_week(35).
 
@@ -72,3 +160,5 @@ room_alloc(r1, '1a', deu, 0).
 room_alloc(r2, '1a', deu, 1).
 
 coupling('1a', deu, 2, 3).
+coupling('1a', mat, 0, 1).
+
