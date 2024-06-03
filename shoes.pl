dc9fe05eaad92a8e420bbcb8417329e8fb360ccd
jcarpio <jose.carpio@gmail.com>
2024-06-03 12:53:25 +0200

    Update shoes.pl

--- shoes.pl
+++ shoes.pl
@@ -60,8 +60,6 @@ requirements_variables(Rs, Vars) :-
 
 % Task slots can overlap and must respect the defined order
 % by task_a_needs_task_b. If a task has several slots, they must be consecutive
-
-
 constrain_task(Rs, Task) :-
         tfilter(task_req(Task), Rs, Sub),
 		pairs_slots(Sub, Vs),
@@ -76,15 +74,12 @@ constrain_worker(Rs, Worker) :-
 		
 % The product does not need its slots to be different. They can be
 % overlap different tasks of a product
-
 constrain_product(Rs, Product) :-
         tfilter(product_req(Product), Rs, Sub),
 		pairs_slots(Sub, Vs),
         all_different(Vs).		
-		
-			
+				
 task_req(T0, req(_P,T1,_W,_N)-_, T) :- =(T0,T1,T).	
-
 
 worker_req(W0, req(_P,_T,W1,_N)-_, W) :- =(W0,W1,W).
 
@@ -124,6 +119,99 @@ days_variables(Days, Vs) :-
         length(Day, SPD),
         maplist(same_length(Day), Days),
         append(Days, Vs).
+
+product_days(Rs, Product, Days) :-
+        days_variables(Days, Vs),
+        tfilter(product_req(Product), Rs, Sub),
+        foldl(v(Sub), Vs, 0, _).
+
+v(Rs, V, N0, N) :-
+        (   member(req(_,Task,_,_)-Times, Rs),
+            member(N0, Times) -> V = task(Task)
+        ;   V = free
+        ),
+        N #= N0 + 1.
+
+worker_days(Rs, Worker, Days) :-
+        days_variables(Days, Vs),
+        tfilter(worker_req(Worker), Rs, Sub),
+        foldl(v_worker(Sub), Vs, 0, _).
+
+v_worker(Rs, V, N0, N) :-
+        (   member(req(Prod, Task,_,_)-Times, Rs),
+            member(N0, Times) -> V = product_task(Prod, Task)
+        ;   V = free
+        ),
+        N #= N0 + 1.
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   Print objects in roster.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */		
+				
+print_products(Rs) :-
+        products(Ps),
+        format_products(Ps, Rs).
+
+format_products([], _).
+format_products([Product|Products], Rs):- 
+        product_days(Rs, Product, Days0),
+        transpose(Days0, Days),
+        format("Product: ~w~2n", [Product]),
+        weekdays_header,
+        align_rows(Days),
+        format_products(Products, Rs).
+
+align_rows([]):-  format("\n\n\n").
+align_rows([R|Rs]):- 
+        align_row(R),
+        format("\n"),
+        align_rows(Rs).
+
+align_row([]).
+align_row([R|Rs]):-
+        align_(R),
+        align_row(Rs).	
+
+align_(free):- format('').
+align_(product_task(P,T)):- format("~w/~w", [P,T]).
+align_(task(T)):- format("~w", [T]).
+align_(verbatim(Element)):- format("~t~w~t~8+", [Element]).
+
+
+print_workers(Rs) :-
+        workers(Ws),
+        format_workers(Ws, Rs).
+
+format_workers([], _).
+format_workers([W|Ws], Rs):-
+        worker_days(Rs, W, Days0),
+          transpose(Days0, Days),
+        format("Worker: ~w~2n", [W]),
+        weekdays_header,
+        align_rows(Days),
+        format_workers(Ws, Rs).
+
+weekdays_header :- 
+        maplist(with_verbatim,
+                  ['Mon','Tue','Wed','Thu','Fri'],
+                  Vs),
+        align_row(Vs),
+        format("~n~`=t~40|~n").
+
+with_verbatim(T, verbatim(T)).		
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   ?- consult('reqs_example.pl'),
+      requirements_variables(Rs, Vs), labeling([ff], Vs), print_products(Rs).
+   %@ Class: 1a
+   %@
+   %@   Mon     Tue     Wed     Thu     Fri
+   %@ ========================================
+   %@   mat     mat     mat     mat     mat
+   %@   eng     eng     eng
+   %@    h       h
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 		
 %---------------------------------------------------------------		
 		
@@ -151,12 +239,7 @@ task glue_sole need task cut_sole and task shape_wool
 
 */
 
-c(hummer_wool, order_wool).
-task_a_needs_task_b(shape_wool, hummer_wool).
-task_a_needs_task_b(glue_sole, cut_sole).
-task_a_needs_task_b(glue_sole, shape_wool).
-
-c(hummer_wool, order_wool).
+task_a_needs_task_b(hummer_wool, order_wool).
 task_a_needs_task_b(shape_wool, hummer_wool).
 task_a_needs_task_b(glue_sole, cut_sole).
 task_a_needs_task_b(glue_sole, shape_wool).
